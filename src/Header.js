f1372b1f5f2f52fa6668b6852b1a846ffc4e9d9c
LeeChiYun <carrick035@gmail.com>
2023-01-09 20:38:43 +0900

    터치범위 늘리기

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const IconButton = (props) => {
+const IconButton = ({ name, bgColor }) => {
   return (
-    <View style={{ paddingHorizontal: 6 }}>
-      <Ionicons name={props.name} size={24} color="black" />
-    </View>
+    <TouchableOpacity
+      hitSlop={{ top: 10, bottom: 10 }}
+      // 스티알 밖에 터치범위 추가로 주는 것
+      style={{ paddingHorizontal: 6, backgroundColor: bgColor }}
+    >
+      <Ionicons name={name} size={24} color="black" />
+    </TouchableOpacity>
   );
 };
 // expo icons
@@ -17,16 +21,15 @@ export default () => {
         flexDirection: "row",
         justifyContent: "space-between",
         paddingVertical: 10,
-        backgroundColor: "white",
       }}
     >
       <Text style={{ fontSize: 22, fontWeight: "bold" }}>친구</Text>
 
       <View style={{ flexDirection: "row" }}>
-        <IconButton name="search-outline" />
-        <IconButton name="person-add-outline" />
-        <IconButton name="md-musical-notes-outline" />
-        <IconButton name="ios-settings-outline" />
+        <IconButton name="search-outline" bgColor="lightblue" />
+        <IconButton name="person-add-outline" bgColor="lightgreen" />
+        <IconButton name="md-musical-notes-outline" bgColor="lightblue" />
+        <IconButton name="ios-settings-outline" bgColor="lightgreen" />
       </View>
     </View>
   );
